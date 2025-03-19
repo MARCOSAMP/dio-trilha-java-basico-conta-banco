e979a0aa7a8580504b664be705c1b85a32ca3a78
Marcos Morais <morais_px@hotmail.com>
2025-03-18 21:23:40 -0300

    adicionando variaveis e expressões condicionais

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World");
+        //TODO: Conhecer e importar a classe Scanner
+        //Exibir mensagens para usuários
+        //Obter pela classe Scanner os valores digitados no terminal
+        // Exibir mensagem conta criada
+
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = 25;
+        double valorSolicitado;
+
+        System.out.println("Informe o valor a ser sacado");
+        valorSolicitado = scanner.nextDouble();
+    
+
+        if (saldo >= valorSolicitado) {
+            System.out.println("Saque efetuado com sucesso!");
+            saldo = saldo - valorSolicitado;
+        }else{
+            System.out.println("Saldo insuficiente");
+        }
+
+        System.out.println("Saldo atual: "+saldo);
     }
 }
